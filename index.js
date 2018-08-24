20e95fcd5ccdbf50033bc07838665c04e5d8400d
Justine Odegbami <justineodegbami@outlook.com>
2018-08-24 13:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function subtract(a, b) {
  a-= b
   return a
 }
+
+function multiply(a, b) {
+ a*= b
+  return a
+}
+
+function divide(a, b) {
+ a/= b
+  return a
+}
+
+function inc(a) {
+ a+= 1
+  return a
+}
+
+function dec(a) {
+ a-= 1
+  return a
+}
